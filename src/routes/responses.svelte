49caf6fe153f10871d058448ffb85f852bf2aaf9
Jökull Sólberg Auðunsson <jokull@solberg.is>
2021-02-02 22:58:43 +0000

    Fewer grid columns

--- src/routes/responses.svelte
+++ src/routes/responses.svelte
@@ -13,7 +13,7 @@
     Loading
   {:else}
     <div
-      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 gap-y-8"
+      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 gap-y-8"
     >
       {#each responses as response}
         <div>
