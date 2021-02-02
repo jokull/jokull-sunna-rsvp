38e3f371841de2511010c46bc6828bbc07fc2df5
Jökull Sólberg Auðunsson <jokull@solberg.is>
2021-02-02 16:50:34 +0000

    Index of responses

---
+++ src/routes/responses.svelte
@@ -0,0 +1,38 @@
+<script>
+  let responses = [];
+  import { onMount } from "svelte";
+
+  onMount(async () => {
+    const res = await fetch("/_responses");
+    responses = await res.json();
+  });
+</script>
+
+<div class="rounded-lg shadow-lg m-8 p-12 bg-white">
+  {#if responses.length === 0}
+    Loading
+  {:else}
+    <div class="grid grid-cols-4 gap-4">
+      {#each responses as response}
+        <div>
+          <div class="font-bold font-lg">{response.email}</div>
+          <div class="font-light text-sm mb-2">S: {response.phone}</div>
+          {#each response.guests as guest}
+            <div>
+              <div>
+                {#if guest.diet === "pescatarian"}
+                  🐟
+                {:else if guest.diet === "vegan"}
+                  🥬
+                {:else if guest.diet === "meat"}
+                  🥩
+                {/if}
+                {guest.name}
+              </div>
+            </div>
+          {/each}
+        </div>
+      {/each}
+    </div>
+  {/if}
+</div>
